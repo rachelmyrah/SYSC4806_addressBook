ec06cf02e9a6c4766074b2dbdd7056518e1ab683
Rachel Myrah <rachelmyrah@LABS.SCE.CARLETON.CA>
2020-02-12 15:46:57 -0500

    a

--- src/main/java/addressBook/AddressBookController.java
+++ src/main/java/addressBook/AddressBookController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,7 +14,15 @@ public class AddressBookController {
     AddressBookRepository repo;
 
     @GetMapping("/book")
-    public String getAddressBook(@RequestParam(name = "id", required = true) int id, Model model) {
+    public String getAddressBook(@RequestParam(name = "id") int id, Model model) {
+        AddressBook addressBook = repo.findById(id);
+        model.addAttribute("addressBook", addressBook);
+        model.addAttribute("buddyInfo", new BuddyInfo());
+        return "addressBook";
+    }
+
+    @PostMapping("/book")
+    public String addBuddyInfo(@RequestParam(name = "id") int id, @ModelAttribute BuddyInfo buddyInfo, Model model) {
         AddressBook addressBook = repo.findById(id);
         model.addAttribute("addressBook", addressBook);
         return "addressBook";
