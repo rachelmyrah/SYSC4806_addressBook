32129369b328a57e35dd95a4e54b127360ea1c3e
Rachel Myrah <rachelmyrah@LABS.SCE.CARLETON.CA>
2020-02-12 14:23:15 -0500

    a

--- src/main/java/addressBook/jQueryController.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "https://address-book-123456789.herokuapp.com/"
-    }).then(function(data) {
-        $('.greeting-id').append(data.name);
-        $('.greeting-content').append(data.address);
-    });
-});
---
+++ src/main/resources/public/scripts/jQueryController.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "https://address-book-123456789.herokuapp.com/"
+    });
+});
